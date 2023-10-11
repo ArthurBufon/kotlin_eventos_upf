2bf37f8902f1125f9ebf2d350123fd6973acda1f
Arthur Bufon <arthurbufon2052@gmail.com>
2023-10-10 21:45:17 -0300

    Added Service class for Evento

--- src/main/kotlin/br/upf/eventos/eventos/controller/EventoController.kt
+++ src/main/kotlin/br/upf/eventos/eventos/controller/EventoController.kt
@@ -2,6 +2,7 @@ package br.upf.eventos.eventos.controller
 
 import br.upf.eventos.eventos.model.Evento
 import br.upf.eventos.eventos.model.StatusEvento
+import br.upf.eventos.eventos.service.EventoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,20 +10,10 @@ import java.time.LocalDate
 
 @RestController
 @RequestMapping("/eventos")
-class EventoController {
+class EventoController(val eventoService: EventoService) {
 
     @GetMapping
     fun listar(): List<Evento>{
-        val hoje = LocalDate.now();
-        val evento = Evento(
-            id = 1,
-            nome = "UPF em Dança",
-            data = hoje,
-            dataInicioInsc = hoje.atStartOfDay().plusDays(10),
-            dataFimInsc = hoje.atStartOfDay().plusDays(20),
-            descricao = "Melhor evento de dança de salão",
-            status = StatusEvento.ABERTO,
-        )
-        return listOf(evento);
+        return eventoService.listar();
     }
 }
---
+++ src/main/kotlin/br/upf/eventos/eventos/service/EventoService.kt
@@ -0,0 +1,22 @@
+package br.upf.eventos.eventos.service
+
+import br.upf.eventos.eventos.model.Evento
+import br.upf.eventos.eventos.model.StatusEvento
+import java.time.LocalDate
+
+class EventoService {
+
+    fun listar(): List<Evento>{
+        val hoje = LocalDate.now();
+        val evento = Evento(
+            id = 1,
+            nome = "UPF em Dança",
+            data = hoje,
+            dataInicioInsc = hoje.atStartOfDay().plusDays(10),
+            dataFimInsc = hoje.atStartOfDay().plusDays(20),
+            descricao = "Melhor evento de dança de salão",
+            status = StatusEvento.ABERTO,
+        )
+        return listOf(evento);
+    }
+}

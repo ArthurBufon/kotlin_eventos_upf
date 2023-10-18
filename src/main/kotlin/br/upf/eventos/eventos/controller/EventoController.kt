da82dfde3341e62ff0369b0d17816e58c6073774
Arthur Bufon <arthurbufon2052@gmail.com>
2023-10-17 21:27:56 -0300

    Returning 204 no content after deleting evento.

--- src/main/kotlin/br/upf/eventos/eventos/controller/EventoController.kt
+++ src/main/kotlin/br/upf/eventos/eventos/controller/EventoController.kt
@@ -6,6 +6,7 @@ import br.upf.eventos.eventos.model.Evento
 import br.upf.eventos.eventos.model.StatusEvento
 import br.upf.eventos.eventos.service.EventoService
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.util.UriComponentsBuilder
 import java.time.LocalDate
@@ -48,6 +50,7 @@ class EventoController(val service: EventoService) {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun destroy(@PathVariable id: Long) {
         service.destroy(id);
     }

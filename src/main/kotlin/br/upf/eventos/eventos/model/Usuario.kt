5a33019d419abb9379b7bbc1e2054225b9f57399
Arthur Bufon <arthurbufon2052@gmail.com>
2023-10-24 20:00:07 -0300

    Notacoes para models

--- src/main/kotlin/br/upf/eventos/eventos/model/Usuario.kt
+++ src/main/kotlin/br/upf/eventos/eventos/model/Usuario.kt
@@ -1,8 +1,17 @@
 package br.upf.eventos.eventos.model
 
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+
 data class Usuario(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val nome: String,
     val cidade: String,
     val telefone: String,
+
+    @OneToMany(mappedBy = "usuario")
+    val inscricoes: List<Inscricao> = listOf()
 )
